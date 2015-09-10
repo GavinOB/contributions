0db8a398a83f9ce3e9e2fe439b9c864397d4bdf5
Gavin O'Brien <geobrien@gmail.com>
2015-09-09 23:28:04 -0400

    general sports category

--- analysis.R
+++ analysis.R
@@ -3,5 +3,8 @@
 athletes <- contr[grepl(".*athlete.*|.*football player.*|.*nfl player.*", contr$contributor_occupation, ignore.case = TRUE), ]
 
 # Party counts
-table(athletes$recipient_party)
+athletes.parties <- table(athletes$recipient_party)
 
+# More general sports category of employers
+sports <- contr[contr$contributor_category == "G6400", ]
+sports.parties <- table(sports$recipient_party)
